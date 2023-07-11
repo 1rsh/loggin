156eb2a6d86573a7d6e4f3d4b0b21b530c31eb36
Irsh Vijay <93649948+1rsh@users.noreply.github.com>
2023-07-12 00:52:59 +0530

    loggin-kgp

--- contentScript.js
+++ contentScript.js
@@ -1,8 +1,104 @@
-chrome.runtime.onMessage.addListener((msg, sender, response) => {
-    if (msg.command === "runCommand") {
-      var receivedCreds = msg.data;
-      console.log(receivedCreds);
-      // Process the received credentials as needed
+console.log("Content Script Succesfully Injected!")
+
+
+var consoleLogHistory = [];
+
+function interceptConsoleLog() {
+  var originalConsoleLog = console.log;
+
+  console.log = function() {
+    var args = Array.from(arguments);
+    consoleLogHistory.push(args);
+
+    // Call the original console.log function
+    originalConsoleLog.apply(console, args);
+  };
+}
+
+
+
+function fetchFromErp(endpoint, user_id) {
+  var xhr = new XMLHttpRequest();
+  const requestURL = "https://erp.iitkgp.ac.in" + endpoint;
+  xhr.open("POST", requestURL, true);
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  var formData = "user_id=" + encodeURIComponent(user_id);
+  xhr.send(formData);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      if (xhr.status === 200) {
+        var responseText = xhr.responseText;
+        var output = responseText;
+      } else {
+        console.error("Request failed with status:", xhr.status);
+        var output = null;
+      }
+      chrome.storage.local.set({'savedQuestion': output}, () => {console.log("Saved Question: ", output);})
     }
-  });
-  
+    return output
+  };
+};
+
+
+
+
+
+
+
+
+chrome.runtime.onMessage.addListener((msg, sender, response) => {
+    var receivedCreds = msg.data;
+    console.log(receivedCreds);
+    // Process the received credentials as needed
+    response("True");
+    var userField = document.getElementById("user_id");
+    const newValue = receivedCreds.rollNo;
+    var nativeInputValueSetter = Object.getOwnPropertyDescriptor(window.HTMLInputElement.prototype, "value").set;
+    nativeInputValueSetter.call(userField, newValue);
+
+    var ev2 = new Event('blur', { bubbles: true});
+    userField.dispatchEvent(ev2);
+
+
+
+
+    userField.value = receivedCreds.rollNo;
+    document.querySelector("#password").value = receivedCreds.pwd;
+    document.querySelector("#question").click();
+    console.log('Waiting...');
+    setTimeout(function () {
+      var retrievedQuestion = document.querySelector("#question").textContent;
+      console.log(retrievedQuestion);
+      let answer;
+      switch (retrievedQuestion){
+        case receivedCreds.Q1:
+          answer = receivedCreds.Q1A;
+          break;
+        
+        case receivedCreds.Q2:
+          answer = receivedCreds.Q2A;
+          break;
+
+        case receivedCreds.Q3:
+          answer = receivedCreds.Q3A;
+          break;
+      }
+      console.log(answer);
+      document.querySelector("#answer").value= answer;
+    }, 1000);
+
+
+    
+    
+    document.querySelector("#getotp1").click();
+    let otp;
+    setTimeout(function () {
+      console.log('Waiting...');
+      otp = prompt("Please enter OTP: ");
+      if(otp!=null){
+        document.querySelector("#email_otp1").value = otp;
+        document.querySelector("#loginFormSubmitButton").click();
+        }
+    }, 1000);
+    
+});
--- popup.js
+++ popup.js
@@ -1,112 +1,111 @@
-let logIn = document.getElementById('logIn');
-let saveCreds = document.getElementById('saveCreds');
-rollNo = document.getElementById('rollNo');
-pwd = document.getElementById('pwd');
-Q1 = document.getElementById('Q1');
-Q1A = document.getElementById('Q1A');
-Q2 = document.getElementById('Q2');
-Q2A = document.getElementById('Q2A');
-Q3 = document.getElementById('Q3');
-Q3A = document.getElementById('Q3A');
+document.addEventListener("DOMContentLoaded", function() {
+  let logIn = document.getElementById('logIn');
+  let saveCreds = document.getElementById('saveCreds');
+  rollNo = document.getElementById('rollNo');
+  pwd = document.getElementById('pwd');
+  Q1 = document.getElementById('Q1');
+  Q1A = document.getElementById('Q1A');
+  Q2 = document.getElementById('Q2');
+  Q2A = document.getElementById('Q2A');
+  Q3 = document.getElementById('Q3');
+  Q3A = document.getElementById('Q3A');
 
 
-const form = document.getElementById('myForm');
-
-
-
-
+  const form = document.getElementById('myForm');
 
 
 
 
-form.addEventListener('submit', (event) => {
-  event.preventDefault(); // Prevent form submission
-
-  const inputs = form.querySelectorAll('input');
-  let isFilled = true;
-
-  inputs.forEach((input) => {
-    if (input.value.trim() === '') {
-      isFilled = false;
-    }});
-
-    if(isFilled){
-        chrome.storage.local.set({
-            'rollNo': rollNo.value,
-            'pwd': pwd.value,
-            'Q1': Q1.value,
-            'Q1A': Q1A.value,
-            'Q2': Q2.value,
-            'Q2A': Q2A.value,
-            'Q3': Q3.value,
-            'Q3A': Q3A.value
-          },()=>{
-              console.log('data had been stored');
-              displaySaved();
-              alert("Saved Successfully!");
-          });
-    }
-});
-
-
-chrome.storage.local.get([
-    'rollNo',
-    'pwd',
-    'Q1',
-    'Q1A',
-    'Q2',
-    'Q2A',
-    'Q3',
-    'Q3A'], (result=>{
-        rollNo.placeholder = result.rollNo ? result.rollNo : "Enter Roll Number";
-        pwd.placeholder = result.pwd ? "\u2022".repeat(result.pwd.length) : "Enter Password";
-        Q1.placeholder = result.Q1 ? result.Q1 : "Enter ERP Q1";
-        Q1A.placeholder = result.Q1A ? "\u2022".repeat(result.Q1A.length) : "Enter ERP A1";
-        Q2.placeholder = result.Q2 ? result.Q2 : "Enter ERP Q2";
-        Q2A.placeholder = result.Q2A ? "\u2022".repeat(result.Q2A.length) : "Enter ERP A2";
-        Q3.placeholder = result.Q3 ? result.Q3 : "Enter ERP Q3";
-        Q3A.placeholder = result.Q3A ? "\u2022".repeat(result.Q3A.length) : "Enter ERP A3";
-    }));
 
 
 
 
-function displaySaved(){
-    rollNo.value = null;
-    pwd.value = null;
-    Q1.value = null;
-    Q1A.value = null;
-    Q2.value = null;
-    Q2A.value = null;
-    Q3.value = null;
-    Q3A.value = null;
-    chrome.storage.local.get([
-        'rollNo',
-        'pwd',
-        'Q1',
-        'Q1A',
-        'Q2',
-        'Q2A',
-        'Q3',
-        'Q3A'], (result=>{
-            rollNo.placeholder = result.rollNo ? result.rollNo : "Enter your Roll Number";
-            pwd.placeholder = result.pwd ? "\u2022".repeat(result.pwd.length) : "Enter Password";
-            Q1.placeholder = result.Q1 ? result.Q1 : "Enter ERP Q1";
-            Q1A.placeholder = result.Q1A ? "\u2022".repeat(result.Q1A.length) : "Enter ERP A1";
-            Q2.placeholder = result.Q2 ? result.Q2 : "Enter ERP Q2";
-            Q2A.placeholder = result.Q2A ? "\u2022".repeat(result.Q2A.length) : "Enter ERP A2";
-            Q3.placeholder = result.Q3 ? result.Q3 : "Enter ERP Q3";
-            Q3A.placeholder = result.Q3A ? "\u2022".repeat(result.Q3A.length) : "Enter ERP A3";
-        }))
-    
-};
+  form.addEventListener('submit', (event) => {
+    event.preventDefault(); // Prevent form submission
+
+    const inputs = form.querySelectorAll('input');
+    let isFilled = true;
+
+    inputs.forEach((input) => {
+      if (input.value.trim() === '') {
+        isFilled = false;
+      }});
+
+      if(isFilled){
+          chrome.storage.local.set({
+              'rollNo': rollNo.value,
+              'pwd': pwd.value,
+              'Q1': Q1.value,
+              'Q1A': Q1A.value,
+              'Q2': Q2.value,
+              'Q2A': Q2A.value,
+              'Q3': Q3.value,
+              'Q3A': Q3A.value
+            },()=>{
+                console.log('data had been stored');
+                displaySaved();
+                alert("Saved Successfully!");
+            });
+      }
+  });
+
+
+  chrome.storage.local.get([
+      'rollNo',
+      'pwd',
+      'Q1',
+      'Q1A',
+      'Q2',
+      'Q2A',
+      'Q3',
+      'Q3A'], (result=>{
+          rollNo.placeholder = result.rollNo ? result.rollNo : "Enter Roll Number";
+          pwd.placeholder = result.pwd ? "\u2022".repeat(result.pwd.length) : "Enter Password";
+          Q1.placeholder = result.Q1 ? result.Q1 : "Enter ERP Q1";
+          Q1A.placeholder = result.Q1A ? "\u2022".repeat(result.Q1A.length) : "Enter ERP A1";
+          Q2.placeholder = result.Q2 ? result.Q2 : "Enter ERP Q2";
+          Q2A.placeholder = result.Q2A ? "\u2022".repeat(result.Q2A.length) : "Enter ERP A2";
+          Q3.placeholder = result.Q3 ? result.Q3 : "Enter ERP Q3";
+          Q3A.placeholder = result.Q3A ? "\u2022".repeat(result.Q3A.length) : "Enter ERP A3";
+      }));
 
 
 
 
-logIn.addEventListener('click', () => {
-    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-      var activeTab = tabs[0];
+  function displaySaved(){
+      rollNo.value = null;
+      pwd.value = null;
+      Q1.value = null;
+      Q1A.value = null;
+      Q2.value = null;
+      Q2A.value = null;
+      Q3.value = null;
+      Q3A.value = null;
+      chrome.storage.local.get([
+          'rollNo',
+          'pwd',
+          'Q1',
+          'Q1A',
+          'Q2',
+          'Q2A',
+          'Q3',
+          'Q3A'], (result=>{
+              rollNo.placeholder = result.rollNo ? result.rollNo : "Enter your Roll Number";
+              pwd.placeholder = result.pwd ? "\u2022".repeat(result.pwd.length) : "Enter Password";
+              Q1.placeholder = result.Q1 ? result.Q1 : "Enter ERP Q1";
+              Q1A.placeholder = result.Q1A ? "\u2022".repeat(result.Q1A.length) : "Enter ERP A1";
+              Q2.placeholder = result.Q2 ? result.Q2 : "Enter ERP Q2";
+              Q2A.placeholder = result.Q2A ? "\u2022".repeat(result.Q2A.length) : "Enter ERP A2";
+              Q3.placeholder = result.Q3 ? result.Q3 : "Enter ERP Q3";
+              Q3A.placeholder = result.Q3A ? "\u2022".repeat(result.Q3A.length) : "Enter ERP A3";
+          }))
+      
+  };
+
+
+
+
+  logIn.addEventListener('click', () => {
       chrome.storage.local.get([
         'rollNo',
         'pwd',
@@ -118,8 +117,11 @@ logIn.addEventListener('click', () => {
         'Q3A'
       ], function (result) {
         var credtoSend = result;
-        chrome.tabs.sendMessage(activeTab.id, { command: "runCommand", data: credtoSend });
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+          chrome.tabs.sendMessage(tabs[0].id, {data: credtoSend}, function(response) {});  
+          setTimeout(result, 500);
+        });
+      
       });
     });
-  });
-  
+});
